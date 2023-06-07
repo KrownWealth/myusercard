8b9cbb75385ce8a397363b10952b21d457788dc6
KrownWealth <abdulramonadeola36@gmail.com>
2023-06-07 17:12:56 +0100

    getting started with react first user card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import adeolaImage from './images/Avater image-1.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//User card component
+
+const User = () =>(
+  <div className='card-wrapper'>
+    <div className='second-wrapper'>
+    <img src={adeolaImage} alt='adeola img' className='avatar-img' />
+     
+    <div className='profile-text'>
+    <h3>Adeola Abdulramon <span><FontAwesomeIcon icon={faCircleCheck} className="my-icon" style={{ color: 'red', fontSize: '24px' }} /> </span> </h3> 
+    
+    <h5 className='role'>Frontend Developer</h5>
+    </div>
+    <div className='skills'>
+      <h2>Skills</h2>
+      <ul className='skills-card'>
+        <li className='each-card'>HTML</li>
+        <li className='each-card'>HTML</li>
+        <li className='each-card'>HTML</li>
+        <li className='each-card' >HTML</li>
+        <li className='each-card' >HTML</li>
+        <li className='each-card' >HTML</li>
+         <li className='each-card' >HTML</li>
+         <li className='each-card'>HTML</li>
+        <li className='each-card'>HTML</li>
+        <li className='each-card' >HTML</li>
+         <li className='each-card'>HTML</li>
+        <li className='each-card'>HTML</li>
+        
+        
+
+      </ul>
+      </div>
+      </div> 
+  </div>
+)
+
+const App = () => (
+  <div className='app'>
+    <User />
+    
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+// we render the App component using the ReactDOM package
+ReactDOM.render(<App />,  rootElement)
